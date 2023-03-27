fc8fff4d22fee9f2d1944d51045b216606b03d23
michittio <nguyennhutrung.nnt@gmail.com>
2023-03-27 10:35:13 +0700

    first commit

---
+++ app/src/main/java/michittio/ueh/firebase/MainActivity.java
@@ -0,0 +1,45 @@
+package michittio.ueh.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String,Object> user = new HashMap<>();
+        user.put("firstname", "Trung");
+        user.put("lastname", "Nguyen");
+        user.put("description", "Đây là Trung");
+
+        firestore.collection("user").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
